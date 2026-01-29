07fe910a9d5a00c903972c44feb124bd6ec056d6
andreguigo <andreguigo@gmail.com>
2026-01-29 01:08:34 -0300

    ajustes no envio do formulário

--- src/config/auth.js
+++ src/config/auth.js
@@ -3,10 +3,6 @@ require('dotenv').config();
 module.exports = {
     secret: process.env.JWT_SECRET_KEY,
     header: process.env.JWT_HEADER_KEY || 'Authorization',
-    // jwtExpiration: 3600,           // 1 hour
-    // jwtRefreshExpiration: 86400,   // 24 hours
-
-    /* for test */
-    jwtExpiration: 60,          // 1 minute
-    jwtRefreshExpiration: 120,  // 2 minutes
+    jwtExpiration: 3600,           // 1 hour
+    jwtRefreshExpiration: 86400,   // 24 hours
 };
